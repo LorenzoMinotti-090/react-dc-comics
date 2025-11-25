1f4418c5f12dcc9d990462fc8fb48329d02677c4
LorenzoMinotti-090 <minottilorenzo@gmail.com>
2025-11-25 14:52:02 +0100

    docs: react-dc-comics

---
+++ src/header.jsx
@@ -0,0 +1,24 @@
+export default function Header() {
+  return (
+    <header className="site-header">
+      <div className="header-container">
+        <img className="logo" src="/assets/dc-logo.png" alt="DC Logo" />
+
+        <nav>
+          <ul className="nav-links">
+            <li>CHARACTERS</li>
+            <li className="active">COMICS</li>
+            <li>MOVIES</li>
+            <li>TV</li>
+            <li>GAMES</li>
+            <li>COLLECTIBLES</li>
+            <li>VIDEOS</li>
+            <li>FANS</li>
+            <li>NEWS</li>
+            <li>SHOP</li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+}
